8ac6c75774f0a1b1984d8b4861dcef4dcbbd72de
fabiogvl <fabiogvl@hotmail.com>
2023-11-14 18:23:39 -0300

    Automação SwagLabs - Selenium

---
+++ README.md
@@ -0,0 +1,37 @@
+Passos para executar essa Automação de Testes
+
+Através desse readme vou está explicando como baixar, configurar e executar esse projeto de automação de testes do site saucedemo, lembrando, esse teste tem o intuito de aprendizado.
+
+URL do site: https://www.saucedemo.com/
+
+Tecnologias do projeto
+
+Linguagem de programação Java 11
+Framework JUnit Jupiter 4.8.2
+Framework Selenium WebDriver 4.8.3
+Framework Cucumber Java 7.11.0
+Framework Cucumber JUnit 7.11.0
+Biblioteca WebDriverManeger 5.5.3
+
+Pré-requisitos
+
+Java JDK 11 instalado
+Configurar as variáveis de ambiente JAVA_HOME
+Visual Studio Code na versão 1.84 ou superior instalado
+Plugin do VS code Extension Pack for Java instalado
+Plugin do VS code Cucumber instalado
+
+Passo a passo
+
+Clique em "code"
+Clique em "Download Zip" para baixar arquivo zipado
+Acesse a área de Downloads do seu PC
+Encontre o arquivo zipado baixado
+Descompacte/extraia a pasta do arquivo zipado
+Abra o Visual Studio Code
+Clique em arquivo/file
+Clique em abrir pasta/open folder
+Escolha a pasta do arquivo descompactado
+No Visual Studio Code, acesse a pasta src>test>runner
+Dentro da pasta runner, acesse o arquvo TestRunner.java
+Ao lado da linha 13, clique no Icone de play para executar o projeto
---
+++ src/test/java/steps/TesteDeLoginSteps.java
@@ -0,0 +1,88 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TesteDeLoginSteps{
+
+    WebDriver robozinho;
+
+    @Before
+    public void inicio(){
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Fabinho\\Desktop\\JavaIDE\\chromedriver.exe");
+
+        robozinho = new ChromeDriver();
+        robozinho.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @After
+    public void fim(){
+
+        robozinho.quit();
+    }
+
+    @Dado("que o usuario esta na tela de login")
+    public void que_o_usuario_esta_na_tela_de_login() {
+
+        robozinho.get("https://www.saucedemo.com");
+
+    }
+
+    @Quando("preenche username com {string} e password com {string}")
+    public void preenche_username_com_e_password_com(String usuario, String senha)  {
+
+        robozinho.findElement(By.id("user-name")).sendKeys(usuario);
+        robozinho.findElement(By.id("password")).sendKeys(senha);
+
+    }
+
+    @E("clica no botao LOGIN")
+    public void clica_no_botao_login() {
+
+        robozinho.findElement(By.id("login-button")).click();
+
+    }
+
+    @Entao("deve ter {string}")
+    public void deve_ter(String resultado) {
+
+        if(resultado.equals("sucesso")){
+
+            Assertions.assertEquals("https://www.saucedemo.com/inventory.html",
+                    robozinho.getCurrentUrl());
+        }
+
+        if(resultado.equals("invalido1")){
+
+            Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service",
+                    robozinho.findElement(By.tagName("h3")).getText());
+        }
+
+        if(resultado.equals("invalido2")){
+
+            Assertions.assertEquals("Epic sadface: Password is required",
+                    robozinho.findElement(By.tagName("h3")).getText());
+        }
+
+        if(resultado.equals("invalido3")){
+
+            Assertions.assertEquals("Epic sadface: Username is required",
+                    robozinho.findElement(By.tagName("h3")).getText());
+        }
+
+    }
+
+}
